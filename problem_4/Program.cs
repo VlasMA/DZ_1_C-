6c81bec76045ad728944b6d09b25586f7bd9e012
Maxim <masya777999@mail.ru>
2022-06-09 20:14:52 +0500

    added the text of task number 4

--- problem_4/Program.cs
+++ problem_4/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите число a: ");
+﻿// Задача 4. написать программу, которая на вход принимает три числа и выдает максимальное.
+Console.Write("Введите число a: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число b: ");
 int numberB = Convert.ToInt32(Console.ReadLine());
