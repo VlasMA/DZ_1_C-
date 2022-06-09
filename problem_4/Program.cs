c5e52933a6a33e6c4bca94c5565d8517b5d58b20
Maxim <masya777999@mail.ru>
2022-06-09 19:53:01 +0500

    finding the maximum of three

---
+++ problem_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число a: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max)
+{
+    max = numberB;
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine("max=" + max);
