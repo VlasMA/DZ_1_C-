3a4e42f32ec0258cd8c02866410a48d85b817257
Maxim <masya777999@mail.ru>
2022-06-09 20:40:26 +0500

    even numbers

---
+++ problem_8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N), а на выход показывает все четные чтслаот 1 до N.
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstNumber = 2;
+
+while (firstNumber <= number)
+{
+    Console.Write( firstNumber + " " ) ;
+    firstNumber = (firstNumber + 2);
+}
