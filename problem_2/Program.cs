dda95fe60af3a9256e327ba5ad6567f471e514c8
Maxim <masya777999@mail.ru>
2022-06-09 19:22:34 +0500

    finding the maximum of two

---
+++ problem_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число, не равное первому: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("max=" + numberA );
+    Console.WriteLine("min=" + numberB );
+}
+else
+{
+    Console.WriteLine("max=" + numberB );
+    Console.WriteLine("min=" + numberA );
+}
